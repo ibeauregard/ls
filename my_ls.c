935058b8d18299b2fb9ed9f27dacafaf4cc49b0f
ian beauregard <beaurega_i@qwasar.io>
2021-01-18 22:18:40 +0000

    First very simple version: display all  arguments provided

--- my_ls.c
+++ my_ls.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+#define FILENAME_SEP '\t'
+
+int main(int argc, char** argv)
 {
-    puts("Roger");
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s%c", argv[i], FILENAME_SEP);
+    }
+    puts("");
     return 0;
 }
