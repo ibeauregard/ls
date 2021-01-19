ebbf41303066bb96945f12973c9be12d35e1eb34
ian beauregard <beaurega_i@qwasar.io>
2021-01-19 23:49:15 +0000

    Build data stuctures for nondirs and dirs

--- my_list.h
+++ my_list.h
@@ -1,19 +1,57 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define FILENAME_SEP '\t'
+#define INVALID_ARG_MESSAGE "my_ls: cannot access '%s': No such file or directory\n"
+
+#ifndef STRUCT_STAT
+#define STRUCT_STAT
+typedef struct stat Stat;
+#endif
 
 #ifndef STRUCT_STRING_ARRAY
 #define STRUCT_STRING_ARRAY
-typedef struct s_string_array
+typedef struct
 {
     uint size;
     char** array;
-} string_array;
+} StringArray;
 #endif
 
-void print(string_array list);
-string_array sort(string_array list);
-void sort_partitions(string_array list, unsigned int i);
+#ifndef STRUCT_FILE
+#define STRUCT_FILE
+typedef struct
+{
+    char* name;
+    bool isdir;
+    struct timespec mtim;
+} File;
+#endif
+
+#ifndef STRUCT_FILE_NODE
+#define STRUCT_FILE_NODE
+typedef struct s_file_node
+{
+    File* file;
+    struct s_file_node* next;
+} FileNode;
+#endif
+
+#ifndef STRUCT_OPERANDS
+#define STRUCT_OPERANDS
+typedef struct
+{
+    uint n_dirs;
+    uint n_nondirs;
+    FileNode* first; 
+} Operands;
+#endif
+
+Operands* initialize_operands();
+File* get_file_from_stat(Stat fileStat, char* name);
+void print(StringArray list);
+StringArray sort(StringArray list);
+void sort_partitions(StringArray list, uint i);
 bool lower_than(char* s1, char* s2);
 void swap(char** p1, char** p2);
--- my_ls.c
+++ my_ls.c
@@ -1,17 +1,91 @@
 #include "my_list.h"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv)
 {
-    string_array files;
-    files.size = argc - 1;
-    files.array = argv + 1;
-    print(sort(files));
+    Stat fileStat;
+    Operands operands;
+    operands.n_dirs = operands.n_nondirs = 0;
+    operands.first = NULL;
+    FileNode *lastSeen = NULL;
+    for (int i = 1; i < argc; i++)
+    {
+        if (stat(argv[i], &fileStat) < 0)
+        {
+            fprintf(stderr, INVALID_ARG_MESSAGE, argv[i]);
+            return 1;
+        }
+        FileNode* node = malloc(sizeof (FileNode));
+        node->file = get_file_from_stat(fileStat, argv[i]);
+        node->next = NULL;
+        if (node->file->isdir)
+        {
+            operands.n_dirs++;
+        }
+        else
+        {
+            operands.n_nondirs++;
+        }
+        if (!operands.first)
+        {
+            operands.first = node;
+            lastSeen = node;
+        }
+        else
+        {
+            lastSeen = lastSeen->next = node;
+        }
+    }
+    printf("No of dirs: %u; no of nondirs: %u\n", operands.n_dirs, operands.n_nondirs);
+    File *directories[operands.n_dirs], *nondirectories[operands.n_nondirs];
+    uint dirIndex = 0, nondirIndex = 0;
+    FileNode* node = operands.first;
+    while (node)
+    {
+        if (node->file->isdir)
+        {
+            printf("Adding %s to directories at index %u\n", node->file->name, dirIndex);
+            directories[dirIndex++] = node->file;
+        }
+        else
+        {
+            printf("Adding %s to non-directories at index %u\n", node->file->name, nondirIndex);
+            nondirectories[nondirIndex++] = node->file;
+        }
+        FileNode* current = node;
+        node = node->next;
+        free(current);
+    }
+    printf("Final dir index: %u\n", dirIndex);
+    printf("Final nondir index: %u\n", nondirIndex);
+    for (uint i = 0; i < dirIndex; i++)
+    {
+        printf("Directory %s\n", directories[i]->name);
+        free(directories[i]);
+    }
+    for (uint i = 0; i < nondirIndex; i++)
+    {
+        printf("Non-directory %s\n", nondirectories[i]->name);
+        free(nondirectories[i]);
+    }
     puts("");
     return 0;
 }
 
-void print(string_array list)
+File* get_file_from_stat(Stat fileStat, char* name)
+{
+    File* file = malloc(sizeof (File));
+    file->name = name;
+    file->isdir = S_ISDIR(fileStat.st_mode);
+    file->mtim = fileStat.st_mtim;
+    return file;
+}
+
+void print(StringArray list)
 {
     for (uint i = 0; i < list.size; i++)
     {
@@ -19,7 +93,7 @@ void print(string_array list)
     }
 }
 
-string_array sort(string_array list)
+StringArray sort(StringArray list)
 {
     if (list.size <= 1)
     {
@@ -40,9 +114,9 @@ string_array sort(string_array list)
     return list;
 }
 
-void sort_partitions(string_array list, unsigned int i)
+void sort_partitions(StringArray list, uint i)
 {
-    string_array partition;
+    StringArray partition;
     partition.size = i;
     partition.array = list.array;
     sort(partition);
