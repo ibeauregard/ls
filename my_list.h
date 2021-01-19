258a302cad7b7a12d7b062ca1cdb33d6304df880
ian beauregard <beaurega_i@qwasar.io>
2021-01-19 00:02:45 +0000

    Implement lexicographic ordering

---
+++ my_list.h
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define FILENAME_SEP '\t'
+
+#ifndef STRUCT_STRING_ARRAY
+#define STRUCT_STRING_ARRAY
+typedef struct s_string_array
+{
+    uint size;
+    char** array;
+} string_array;
+#endif
+
+void print(string_array* list);
+string_array* sorted(string_array* list);
+void sort_partitions(string_array* list, unsigned int i);
+bool smaller_than(char* s1, char* s2);
+void swap(char** p1, char** p2);
--- my_ls.c
+++ my_ls.c
@@ -1,13 +1,67 @@
+#include "my_list.h"
 #include <stdio.h>
-
-#define FILENAME_SEP '\t'
 
 int main(int argc, char** argv)
 {
-    for (int i = 1; i < argc; i++)
-    {
-        printf("%s%c", argv[i], FILENAME_SEP);
-    }
+    string_array* files = malloc(sizeof (string_array));
+    files->size = argc - 1;
+    files->array = argv + 1;
+    print(sorted(files));
+    free(files);
     puts("");
     return 0;
+}
+
+void print(string_array* list)
+{
+    for (uint i = 0; i < list->size; i++)
+    {
+        printf("%s%c", list->array[i], FILENAME_SEP);
+    }
+}
+
+string_array* sorted(string_array* list)
+{
+    if (list->size <= 1)
+    {
+        return list;
+    }
+    int i = 0;
+    char* pivot = list->array[list->size - 1];
+    for (uint j = 0; j < list->size - 1; j++)
+    {
+        if (smaller_than(list->array[j], pivot))
+        {
+            swap(&list->array[i], &list->array[j]);
+            i++;
+        }
+    }
+    swap(&list->array[i], &list->array[list->size - 1]);
+    sort_partitions(list, i);
+    return list;
+}
+
+void sort_partitions(string_array* list, unsigned int i)
+{
+    string_array* partition = malloc(sizeof (string_array));
+    partition->size = i;
+    partition->array = list->array;
+    sorted(partition);
+    partition->size = list->size - (i + 1);
+    partition->array = list->array + i + 1;
+    sorted(partition);
+    free(partition);
+}
+
+bool smaller_than(char* s1, char* s2)
+{
+    for (; *s1 && *s2 && *s1 == *s2; s1++, s2++);
+    return *s1 < *s2;
+}
+
+void swap(char** p1, char** p2)
+{
+    char* temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
 }
