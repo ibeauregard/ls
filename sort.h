12913308a20056ecc41f2fc7cc5385d8bf5650a6
ian beauregard <beaurega_i@qwasar.io>
2021-01-21 18:37:08 +0000

    Make some sort functions static

--- sort.h
+++ sort.h
@@ -2,9 +2,3 @@
 #include "file_array.h"
 
 FileArray* sort(FileArray* files, bool time);
-void sort_partitions(const FileArray* whole, uint i, bool time);
-bool file_lower_than(const File* f1, const File* f2, bool time);
-bool file_path_lower_than(const File* f1, const File* f2);
-bool file_time_lower_than(const File* f1, const File* f2);
-bool string_lower_than(const char* s1, const char* s2);
-void swap(File** f1, File** f2);
