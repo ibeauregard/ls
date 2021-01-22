924d41d022894e0c3323e02b8dd2068c58a5277a
ian beauregard <beaurega_i@qwasar.io>
2021-01-22 00:45:54 +0000

    Better allocation of file array when walking directory

--- print.c
+++ print.c
@@ -7,10 +7,11 @@
 static void print_files(FileArray* files, SortKey* sortKey);
 static void print_directories(FileArray* dirs, bool nondirs, PrintOptions* options);
 static void print_dircontent(const File* directory, PrintOptions* options);
-static uint count_files(const File* directory);
-static void parse_folder(DIR* folder, const char* path, uint n_files, bool show_hidden, FileArray* output);
+static uint count_files(const File* directory, bool showHidden);
+static void parse_folder(DIR* folder, const char* path, uint n_files, bool showHidden, FileArray* output);
 static void print_newline();
 static char* build_path(char* fullpath, const char* dirpath, const char* name);
+static bool considered(const char* name, bool showHidden);
 
 void print(FileArray* files, FileArray* directories, PrintOptions* options)
 {
@@ -60,7 +61,7 @@ static void print_directories(FileArray* dirs, bool nondirs, PrintOptions* optio
 
 static void print_dircontent(const File* directory, PrintOptions* options)
 {
-    uint n_files = count_files(directory);
+    uint n_files = count_files(directory, options->showHidden);
     FileArray files;
     DIR* folder = opendir(directory->path);
     parse_folder(folder, directory->path, n_files, options->showHidden, &files);
@@ -68,25 +69,31 @@ static void print_dircontent(const File* directory, PrintOptions* options)
     closedir(folder);
 }
 
-static uint count_files(const File* directory)
+static uint count_files(const File* directory, bool showHidden)
 {
-    uint n_files;
+    uint n_files = 0;
     DIR* folder = opendir(directory->path);
-    for (n_files = 0; readdir(folder); n_files++);
+    Dirent* entry;
+    while ((entry = readdir(folder)))
+    {
+        if (considered(entry->d_name, showHidden))
+        {
+            n_files++;
+        }
+    }
     closedir(folder);
     return n_files;
 }
 
-static void parse_folder(DIR* folder, const char* path, uint n_files, bool show_hidden, FileArray* output)
+static void parse_folder(DIR* folder, const char* path, uint n_files, bool showHidden, FileArray* output)
 {
     Dirent* entry;
     uint i = 0;
     initialize_file_array(output, n_files);
     while ((entry = readdir(folder)))
     {
-        if (starts_with(entry->d_name, HIDDEN_FILE_CHAR) && !show_hidden)
+        if (!considered(entry->d_name, showHidden))
         {
-            output->size--;
             continue;
         }
         Stat fileStat;
@@ -107,4 +114,9 @@ static void print_newline()
 static char* build_path(char* fullpath, const char* dirpath, const char* name)
 {
     return _strcat(_strcat(_strcpy(fullpath, dirpath), PATH_SEP), name);   
+}
+
+static bool considered(const char* name, bool showHidden)
+{
+    return !starts_with(name, HIDDEN_FILE_CHAR) || showHidden;
 }
